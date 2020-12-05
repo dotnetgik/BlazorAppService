44650120cf78028e512abf12d315545c1059e80f
Mangesh Gaherwar <mangesh.gaherwar@smartlockr.eu>
2020-12-05 11:31:06 +0530

    Initial Commit

--- BlazorAppService/Program.cs
+++ BlazorAppService/Program.cs
@@ -19,6 +19,7 @@ namespace BlazorAppService
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
             await builder.Build().RunAsync();
         }
     }
